f28e5082381fe767147cc719a665db2e67c1a330
Alon Hearter <alon.hearter@gmail.com>
2023-08-08 09:53:32 +0300

    fix cht.sh

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -10,9 +10,9 @@ fi
 
 read -p "Query: " query
 
-if grep -qs "$selected" ~/.conig/cht.sh/languages; then
+if grep -qs "$selected" ~/.config/cht.sh/languages; then
   query=$(echo $query | tr " " "+")
-  tmux neww bash -c "curl cht.sh/$selected/$query | less -r"
+  tmux neww bash -c "curl -s cht.sh/$selected/$query | less -r"
 else
-  tmux neww bash -c "curl cht.sh/$selected~$query | less -r"
+  tmux neww bash -c "curl -s cht.sh/$selected~$query | less -r"
 fi
