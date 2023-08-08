7c2a87f7808be5db4267e89ad2a5bdf5c06b18de
Alon Hearter <alon.hearter@gmail.com>
2023-08-08 15:14:48 +0300

    telescope hidden files actually works

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,10 @@ return {
     pickers = {
       find_files = {
         -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        additional_args = { "--hidden", "-g=!**/.git/*" },
       },
       live_grep = {
-        cmd = { "rg", "--column", "--line-number", "--no-heading", "--color=never", "--hidden", "-g", "!**/.git/*" },
+        additional_args = { "--hidden", "-g=!**/.git/*" },
       },
     },
   },
