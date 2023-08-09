76db523c7b575ebea198f25b707b814e1e6683fd
Alon Hearter <alon.hearter@gmail.com>
2023-08-09 18:36:59 +0300

    neotest

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
+    opts = function (_, opts)
+      vim.list_extend(opts.ensure_installed, {
         "hcl",
         "terraform",
         "bash",
@@ -20,7 +20,10 @@ return {
         "typescript",
         "vim",
         "yaml",
-      },
-    },
+        "ron",
+        "rust",
+        "toml"
+      })
+    end,
   },
 }
