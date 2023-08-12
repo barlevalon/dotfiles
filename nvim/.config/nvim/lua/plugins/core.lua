d72314ef46eebb4783f969dfc92a49a6feb2a290
Alon Hearter <alon.hearter@gmail.com>
2023-08-12 10:21:06 +0300

    catppuccin

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,4 +1,10 @@
 return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = { "catppuccin-macchiato" }
+    }
+  },
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
