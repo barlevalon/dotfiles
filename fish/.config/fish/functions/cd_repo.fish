034d7e50a8b539a268ac1c3852e3445d039434f8
Alon Hearter <alon.hearter@gmail.com>
2023-08-21 12:06:53 +0300

    Adding some github functionality

--- fish/.config/fish/functions/cd_repo.fish
+++ fish/.config/fish/functions/cd_repo.fish
@@ -1,9 +1,9 @@
 function cd_repo
-  set repos (find ~/repos ~/repos/sage -type d  -mindepth 1 -maxdepth 1 | sed "s#$HOME/repos/##" | grep -v "DS_Store")
+  set repos (find ~/personal ~/work -type d  -mindepth 1 -maxdepth 1 | sed "s#$HOME/##" | grep -v "DS_Store")
 
   set selected (echo $repos | string split -n " " | gum filter --height 10 --indicator="→")
   
   if test -n "$selected"
-    cd "$HOME/repos/$selected" || exit
+    cd "$HOME/$selected" || exit
   end
 end
