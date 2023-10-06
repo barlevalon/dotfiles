d052585f6408c324865a86791cf5c14c39d37390
Alon Hearter <alon.hearter@gmail.com>
2023-10-06 12:58:23 +0300

    chore: none-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = { "BufRead", "BufNewFile" },
     dependencies = {"mason.nvim"},
     opts = function ()
