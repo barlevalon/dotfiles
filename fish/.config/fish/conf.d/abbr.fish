d53cd09d05df9d430942196b44909016718074b1
Alon Hearter <alon.hearter@gmail.com>
2023-11-06 21:58:38 +0200

    chore: clean up abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,12 +1,2 @@
-#chezmoi
-abbr cm 'chezmoi'
-abbr cms 'chezmoi status'
-abbr cme 'chezmoi edit --apply'
-abbr cma 'chezmoi add'
-abbr cmd 'chezmoi diff'
-
-#ssh through kitty
-abbr ssh 'kitty +kitten ssh'
-
 abbr k 'kubectl'
 abbr tf 'terraform'
