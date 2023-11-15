6ce72b0a9d08245e5c39a715c2d502f1ecca21d3
Alon Hearter <alon.hearter@gmail.com>
2023-11-15 14:18:59 +0200

    feat(nvim): keymaps for inc/dec and select all

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+
 
 -- Move Lines
-vim.keymap.set("n", "<leader>j", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<leader>k", "<cmd>m .-2<cr>==", { desc = "Move up" })
+keymap.set("n", "<leader>j", "<cmd>m .+1<cr>==", { desc = "Move down" })
+keymap.set("n", "<leader>k", "<cmd>m .-2<cr>==", { desc = "Move up" })
 -- vim.keymap.set("i", "<leader>j", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 -- vim.keymap.set("i", "<leader>k", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "<leader>j", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "<leader>k", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-vim.keymap.set("n", "<A-\\>", ":silent !tmux neww tmux-sessionizer<CR>", { silent=true, desc = "tmux session" })
+keymap.set("v", "<leader>j", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+keymap.set("v", "<leader>k", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+keymap.set("n", "<A-\\>", ":silent !tmux neww tmux-sessionizer<CR>", { silent=true, desc = "tmux session" })
 
-vim.keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Prev buffer" })
+keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Next buffer" })
+keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Prev buffer" })
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>", { desc = "Increment number under cursor" })
+keymap.set("n", "-", "<C-x>", { desc = "Decrement number under cursor" })
+
+-- Select all
+keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
