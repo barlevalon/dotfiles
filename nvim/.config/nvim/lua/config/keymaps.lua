49d3357de74ff1564386cd2f873ddff076dced1c
Alon Hearter <alon.hearter@gmail.com>
2023-12-01 16:15:27 +0200

    fix(nvim): move lines keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,13 +4,9 @@
 local keymap = vim.keymap
 
 -- Move Lines
-keymap.set("n", "<leader>j", "<cmd>m .+1<cr>==", { desc = "Move down" })
-keymap.set("n", "<leader>k", "<cmd>m .-2<cr>==", { desc = "Move up" })
--- vim.keymap.set("i", "<leader>j", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
--- vim.keymap.set("i", "<leader>k", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-keymap.set("v", "<leader>j", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-keymap.set("v", "<leader>k", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
+keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move current line up
 keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Next buffer", silent = true })
 keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Prev buffer", silent = true })
 
