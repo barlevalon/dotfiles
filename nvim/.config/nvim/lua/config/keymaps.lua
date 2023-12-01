0c6370f8ec03c98e5790ab4c89eed37a4d967471
Alon Hearter <alon.hearter@gmail.com>
2023-12-01 21:09:37 +0200

    feat(nvim): keymap to replace highlighted word

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,10 @@
 local keymap = vim.keymap
 
 -- Move Lines
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move current line(s) down" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move current line(s) up" })
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
-keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move current line up
+-- Buffers
 keymap.set("n", "<Tab>", ":bn<CR>", { desc = "Next buffer", silent = true })
 keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Prev buffer", silent = true })
 
@@ -16,3 +17,6 @@ keymap.set("n", "-", "<C-x>", { desc = "Decrement number under cursor" })
 
 -- Select all
 keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+-- Replace all instances of highlighted words
+keymap.set("v", "<leader>rr", '"hy:%s/<C-r>h//g<left><left>', { desc = "Replace all instances of highlighted words" })
