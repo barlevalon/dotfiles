3bc7180418e562cee7734d2f558fe852638151de
Alon Hearter <alon.hearter@gmail.com>
2023-12-12 10:21:50 +0200

    chore: wezterm clean

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 local wezterm = require("wezterm")
 
 local function scheme_for_appearance(appearance)
-	-- if true then
-	-- 	return "Catppuccin Macchiato"
-	-- end
 	if appearance:find("Dark") then
 		return "Catppuccin Macchiato"
 	else
@@ -11,19 +8,16 @@ local function scheme_for_appearance(appearance)
 	end
 end
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.font_size = 18
+config.font = wezterm.font({
+	family = "JetBrainsMono Nerd Font",
+})
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -37,5 +31,5 @@ config.default_cursor_style = "BlinkingBar"
 config.keys = {
 	{ key = "L", mods = "CTRL|ALT", action = wezterm.action.ShowDebugOverlay },
 }
--- and finally, return the configuration to wezterm
+
 return config
