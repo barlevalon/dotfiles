81f28440d965d0fac7d8b6c5444286d6dd2d675d
Alon Hearter <alon.hearter@gmail.com>
2024-01-05 19:40:22 +0200

    fix(nvim): some ui stuff

--- nvim/.config/nvim-scratch/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim-scratch/lua/plugins/catppuccin.lua
@@ -1,28 +1,30 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	lazy = false,
 	priority = 1000,
-	opts = {
-		background = {
-			light = "latte",
-			dark = "macchiato",
-		},
-		transparent_background = true,
-		integrations = {
-			alpha = true,
-			mason = true,
-			neotest = true,
-			lsp_trouble = true,
-			illuminate = {
-				enabled = true,
-				lsp = false,
-			},
-			which_key = true,
-			neotree = true,
-		},
-	},
 	config = function()
-		vim.cmd([[colorscheme catppuccin]])
+		require("catppuccin").setup({
+			transparent_background = true,
+			term_colors = true,
+			flavour = "macchiato",
+			background = {
+				light = "latte",
+				dark = "macchiato",
+			},
+			integrations = {
+				alpha = true,
+				mason = true,
+				neotest = true,
+				lsp_trouble = true,
+				illuminate = {
+					enabled = true,
+					lsp = false,
+				},
+				which_key = true,
+				neotree = true,
+				notify = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
