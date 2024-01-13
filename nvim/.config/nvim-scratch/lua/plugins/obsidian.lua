fa9759f1ceea3800a6036550005579ac57516bd6
Alon Hearter <alon.hearter@gmail.com>
2024-01-13 14:26:22 +0200

    nvim obsidian

---
+++ nvim/.config/nvim-scratch/lua/plugins/obsidian.lua
@@ -0,0 +1,28 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	event = {
+    -- stylua: ignore
+		"BufReadPre " .. vim.fn.expand("~") .. "/notes/**.md",
+		"BufNewFile " .. vim.fn.expand("~") .. "/notes/**.md",
+	},
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-telescope/telescope.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "notes",
+				path = "~/notes",
+			},
+		},
+		daily_notes = {
+			folder = "dailies",
+		},
+	},
+}
--- nvim/.config/nvim-scratch/lua/plugins/treesitter.lua
+++ nvim/.config/nvim-scratch/lua/plugins/treesitter.lua
@@ -1,10 +1,15 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-  config = function ()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"markdown",
+				"markdown_inline",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
