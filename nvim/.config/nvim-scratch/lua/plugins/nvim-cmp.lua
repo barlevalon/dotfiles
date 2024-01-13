0928a28da29f9087e07ee6dee896cd1750ff168d
Alon Hearter <alon.hearter@gmail.com>
2024-01-13 09:08:29 +0200

    nvim cmp emoji

--- nvim/.config/nvim-scratch/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim-scratch/lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-emoji",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
@@ -46,6 +47,7 @@ return {
 				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
+				{ name = "emoji" },
 			}),
 			formatting = {
 				format = lspkind.cmp_format({
