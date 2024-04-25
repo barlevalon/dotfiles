55b7556f5a5b0ee16e94efa3d6bc28a99865a987
Alon Hearter <alon.hearter@gmail.com>
2024-04-25 14:09:38 +0300

    nvim don't auto-save special buffers

--- nvim/.config/nvim-scratch/lua/plugins/auto-save.lua
+++ nvim/.config/nvim-scratch/lua/plugins/auto-save.lua
@@ -1,8 +1,17 @@
 return {
-  "okuuva/auto-save.nvim",
-  opts = {
-    execution_message = {
-      enabled = false
-    }
-  }
+	"okuuva/auto-save.nvim",
+	opts = {
+		execution_message = {
+			enabled = false,
+		},
+		condition = function(buf)
+			local fn = vim.fn
+
+			-- don't save for special-buffers
+			if fn.getbufvar(buf, "&buftype") ~= "" then
+				return false
+			end
+			return true
+		end,
+	},
 }
