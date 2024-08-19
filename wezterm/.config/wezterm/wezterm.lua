67657015c30cf9974d12d00808594c2346b009f4
Alon Hearter <barlevalon@users.noreply.github.com>
2024-08-19 13:24:47 +0300

    wezterm remove background transparency

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -24,9 +24,6 @@ config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
 config.default_cursor_style = "BlinkingBar"
 
 config.keys = {
