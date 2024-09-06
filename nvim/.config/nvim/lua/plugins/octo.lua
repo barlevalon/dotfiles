0b2df331386433393a1cc78c769e150dc2482c37
Alon Hearter <barlevalon@users.noreply.github.com>
2024-09-06 19:32:15 +0300

    nvim octo use gh with op

--- nvim/.config/nvim/lua/plugins/octo.lua
+++ nvim/.config/nvim/lua/plugins/octo.lua
@@ -7,7 +7,16 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("octo").setup({ enable_builtin = true })
+		require("octo").setup({
+			enable_builtin = true,
+			gh_env = function()
+				local github_token = require("op").get_secret("Github", "token")
+				if not github_token or not vim.startswith(github_token, "ghp_") then
+					error("Failed to get GitHub token.")
+				end
+				return { GITHUB_TOKEN = github_token }
+			end,
+		})
 		vim.cmd([[hi OctoEditable guibg=none]])
 	end,
 	keys = {
---
+++ nvim/.config/nvim/lua/plugins/op.lua
@@ -0,0 +1,5 @@
+return {
+	"mrjones2014/op.nvim",
+	build = "make install",
+	config = true,
+}
