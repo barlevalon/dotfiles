da3fbdff49ff7e087c37480fc803310474dd29cc
Alon Hearter <barlevalon@users.noreply.github.com>
2025-01-13 15:45:34 +0200

    delete nvim-lazy

--- nvim/.config/nvim-lazy/lua/plugins/auto-save.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "okuuva/auto-save.nvim",
-  opts = {
-    execution_message = {
-      enabled = false
-    }
-  }
-}
--- nvim/.config/nvim-lazy/lua/plugins/dark-notify.lua
+++
@@ -1,8 +0,0 @@
-local dn = function()
-  require("dark_notify").run()
-end
-return {
-  "cormacrelf/dark-notify",
-  config = dn,
-  init = dn,
-}
