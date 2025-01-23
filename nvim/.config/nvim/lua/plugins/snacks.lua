18704cbf157e2c49118818b999b86da20126f224
Alon Hearter <barlevalon@users.noreply.github.com>
2025-01-24 00:16:47 +0200

    lazygit from snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	---@type snacks.Config
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
@@ -11,5 +12,16 @@ return {
 		quickfile = { enabled = true },
 		-- statuscolumn = { enabled = true },
 		-- words = { enabled = true },
+		lazygit = {},
+	},
+	keys = {
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit.open()
+			end,
+			desc = "LazyGit",
+			silent = true,
+		},
 	},
 }
