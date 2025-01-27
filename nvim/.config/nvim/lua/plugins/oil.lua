afb33633436d35527f8bb1a31d36429138c151cc
Alon Hearter <barlevalon@users.noreply.github.com>
2025-01-27 15:46:45 +0200

    some oil configs

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,10 +4,16 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		float = {
+			preview_split = "right",
+		},
+		keymaps = {
+			["q"] = { "actions.close", mode = "n" },
+		},
 	},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "<leader>o", "<cmd>Oil<cr>", desc = "Oil" },
+		{ "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil" },
 	},
 }
