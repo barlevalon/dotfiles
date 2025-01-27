42c2f1841177b2222916d550d76dabf64d542d7d
Alon Hearter <barlevalon@users.noreply.github.com>
2025-01-27 17:49:17 +0200

    nvim alpha -> snacks dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -13,6 +13,34 @@ return {
 		-- statuscolumn = { enabled = true },
 		-- words = { enabled = true },
 		lazygit = {},
+		dashboard = {
+			sections = {
+				{ section = "header" },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Recent Files",
+					section = "recent_files",
+					indent = 2,
+					padding = 1,
+				},
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
+			},
+		},
 	},
 	keys = {
 		{
