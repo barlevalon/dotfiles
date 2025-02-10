db2a358a4d2954fb09bc69b3749b96bb0237546d
Alon Hearter <barlevalon@users.noreply.github.com>
2025-02-10 11:31:14 +0200

    nvim highlight_yank

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -29,3 +29,12 @@ vim.g.clipboard = {
 		["*"] = "pbpaste",
 	},
 }
+
+local highlight_yank = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+	end,
+	group = highlight_yank,
+})
