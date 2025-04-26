3dbb0ab324b7d99c35d7dca8be3bc5940dade75e
Alon Hearter <barlevalon@users.noreply.github.com>
2025-04-26 19:16:09 +0300

    qutebrowser config

---
+++ qutebrowser/.qutebrowser/config.py
@@ -0,0 +1,16 @@
+config.load_autoconfig()
+
+c.tabs.position = "top"
+c.tabs.show = "always"
+c.fonts.default_size = "20pt"
+
+c.auto_save.session = True
+c.input.insert_mode.auto_load = True
+
+en_keys = "qwertyuiopasdfghjkl'zxcvbnm,./"
+he_keys = '/׳קראטוןםפשדגכעיחלך,זסבהנמצתץ.'
+c.bindings.key_mappings.update(dict(zip(he_keys, en_keys)))
+
+c.content.user_stylesheets = ["~/.qutebrowser/hebrew-fonts.css"]
+
+c.bindings.key_mappings = {'<Ctrl-Escape>':':fake-key <esc>'}
