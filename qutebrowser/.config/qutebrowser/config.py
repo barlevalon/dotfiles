3b535c8b1c89319cd2f2d330eae46d0bbb110c71
Alon Hearter <barlevalon@users.noreply.github.com>
2025-07-10 22:03:21 +0300

    qutebrowser: add 1password integration with qute-1pass

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -9,11 +9,19 @@ c.input.insert_mode.auto_load = True
 
 en_keys = "qwertyuiopasdfghjkl'zxcvbnm,./"
 he_keys = '/׳קראטוןםפשדגכעיחלך,זסבהנמצתץ.'
+
+# Initialize key mappings
+c.bindings.key_mappings = {'<Ctrl-Escape>':':fake-key <esc>'}
+# Add Hebrew key mappings
 c.bindings.key_mappings.update(dict(zip(he_keys, en_keys)))
 
 c.content.user_stylesheets = ["~/.config/qutebrowser/hebrew-fonts.css"]
 
-c.bindings.key_mappings = {'<Ctrl-Escape>':':fake-key <esc>'}
-
 # Enable GPU hardware acceleration
 c.qt.args = ["enable-features=VaapiVideoDecoder,VaapiVideoEncoder", "enable-gpu-rasterization", "enable-zero-copy"]
+
+# 1Password integration keybindings
+config.bind('<Alt-p>', 'spawn --userscript qute-1pass')
+config.bind('<Alt-Shift-p>', 'spawn --userscript qute-1pass --username-only')
+config.bind('<Alt-Ctrl-p>', 'spawn --userscript qute-1pass --password-only')
+config.bind('<Alt-t>', 'spawn --userscript qute-1pass --totp-only')
