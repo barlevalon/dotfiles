40a65438fd45976544374d452ef5427477d09d55
Alon Hearter <barlevalon@users.noreply.github.com>
2025-07-13 17:06:42 +0300

    Fix fish startup error with enable_transience
    - Move enable_transience to starship.fish after starship init
    - Remove empty 99-misc.fish file
    - Ensures transience is only enabled after Starship is loaded

--- fish/.config/fish/conf.d/99-misc.fish
+++
@@ -1,2 +0,0 @@
-# Enable transient prompt
-enable_transience
--- fish/.config/fish/conf.d/starship.fish
+++ fish/.config/fish/conf.d/starship.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
 if command -q starship
     starship init fish | source
+    # Enable transient prompt to reduce clutter in scrollback
+    enable_transience
 end
