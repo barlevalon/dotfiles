34863b30c1fa9e7f6b0f96169b45b25a36a93696
Alon Hearter <barlevalon@users.noreply.github.com>
2025-07-17 22:41:00 +0300

    uwsm

--- fish/.config/fish/conf.d/10-hyprland.fish
+++ fish/.config/fish/conf.d/10-hyprland.fish
@@ -1,4 +1,6 @@
-# Start Hyprland on tty1
+# Start Hyprland on tty1 using uwsm for session management
 if test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
-    exec Hyprland
+    # Clear screen to prevent terminal flash during transition
+    clear
+    exec uwsm start hyprland-uwsm.desktop
 end
