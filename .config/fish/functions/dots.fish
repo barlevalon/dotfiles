e6ee6c59d25bf1dea3788de2ead57ad1f22b8656
Alon Hearter <barlevalon@users.noreply.github.com>
2025-08-12 23:11:52 +0300

    fix ldots by moving to its own file

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,7 +1,3 @@
 function dots -w git
  git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
 end
-
-function ldots -w lazygit
-  lazygit --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
-end
---
+++ .config/fish/functions/ldots.fish
@@ -0,0 +1,4 @@
+function ldots -w lazygit
+  lazygit --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
+end
+
