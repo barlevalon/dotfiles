332edbec911d1b79a8057b62fd6e56aad5abacf6
Alon Hearter <barlevalon@users.noreply.github.com>
2025-08-21 11:44:15 +0300

    nvim: clean up lspconfig and remove lazy-lock.json from tracking

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
+		"b0o/schemastore.nvim",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
 	config = function()
@@ -12,78 +13,74 @@ return {
 		vim.keymap.del("x", "gra")
 		vim.keymap.del("n", "grn")
 		vim.keymap.del("n", "gri")
+		vim.keymap.del("n", "grt")
 
-		local lspconfig = require("lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local keymap = vim.keymap
-
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
-
-			opts.desc = "Hover"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-			opts.desc = "Definitions"
-			keymap.set("n", "gd", function()
-				Snacks.picker.lsp_definitions()
-			end, opts)
-
-			opts.desc = "Implementations"
-			keymap.set("n", "gi", function()
-				Snacks.picker.lsp_implementations()
-			end, opts)
-
-			opts.desc = "References"
-			keymap.set("n", "gr", function()
-				Snacks.picker.lsp_references()
-			end, opts)
-
-			opts.desc = "Actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-			opts.desc = "Rename"
-			keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-
-			opts.desc = "File Diagnostics"
-			keymap.set("n", "<leader>D", function()
-				Snacks.picker.diagnostics({ current = true })
-			end, opts)
-
-			opts.desc = "Line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-
-			opts.desc = "Prev diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-			opts.desc = "Next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-		end
-
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- Configure diagnostic signs
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		lspconfig.terraformls.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
+		-- Set up keymaps when LSP attaches to a buffer
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+			callback = function(event)
+				local opts = { buffer = event.buf, noremap = true, silent = true }
+				
+				opts.desc = "Hover"
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+				opts.desc = "Definitions"
+				vim.keymap.set("n", "gd", function()
+					Snacks.picker.lsp_definitions()
+				end, opts)
+
+				opts.desc = "Implementations"
+				vim.keymap.set("n", "gi", function()
+					Snacks.picker.lsp_implementations()
+				end, opts)
+
+				opts.desc = "References"
+				vim.keymap.set("n", "gr", function()
+					Snacks.picker.lsp_references()
+				end, opts)
+
+				opts.desc = "Actions"
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+				opts.desc = "Rename"
+				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+
+				opts.desc = "File Diagnostics"
+				vim.keymap.set("n", "<leader>D", function()
+					Snacks.picker.diagnostics({ current = true })
+				end, opts)
+
+				opts.desc = "Line diagnostics"
+				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+				opts.desc = "Prev diagnostic"
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+				opts.desc = "Next diagnostic"
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+				opts.desc = "Restart LSP"
+				vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+			end,
 		})
 
-		lspconfig.ts_ls.setup({
+		-- Configure specific servers with custom settings
+		-- These will be picked up automatically when servers are enabled
+		local lspconfig = require("lspconfig")
+		
+		-- Configure servers that need special settings
+		lspconfig.lua_ls.setup({
 			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
 			settings = {
 				Lua = {
 					diagnostics = {
@@ -99,17 +96,42 @@ return {
 			},
 		})
 
-		-- Disable golangci_lint_ls entirely
-		lspconfig.golangci_lint_ls = nil
-
-		-- Set default handler for auto-started servers to use our on_attach
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(event)
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.name == "gopls" then
-					on_attach(client, event.buf)
-				end
-			end,
+		lspconfig.yamlls.setup({
+			capabilities = capabilities,
+			settings = {
+				yaml = {
+					schemaStore = {
+						enable = false,
+						url = "",
+					},
+					schemas = require("schemastore").yaml.schemas(),
+				},
+			},
 		})
+
+		lspconfig.jsonls.setup({
+			capabilities = capabilities,
+			settings = {
+				json = {
+					schemas = require("schemastore").json.schemas(),
+					validate = { enable = true },
+				},
+			},
+		})
+
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		-- Setup each LSP server with our common on_attach and capabilities
+		-- This approach avoids the duplicate server issue by using LspAttach autocmd
+		
+		-- Store common config for reuse
+		_G.lsp_common_on_attach = on_attach
+		_G.lsp_common_capabilities = capabilities
 	end,
 }
--- .config/nvim/lua/plugins/schemastore.lua
+++ .config/nvim/lua/plugins/schemastore.lua
@@ -1,37 +1,5 @@
 return {
 	"b0o/schemastore.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		
-		-- Configure YAML language server
-		lspconfig.yamlls.setup({
-			settings = {
-				yaml = {
-					schemaStore = {
-						-- Disable built-in schemaStore support
-						enable = false,
-						-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-						url = "",
-					},
-					-- Use schemastore.nvim instead
-					schemas = require("schemastore").yaml.schemas(),
-				},
-			},
-		})
-		
-		-- Configure JSON language server
-		lspconfig.jsonls.setup({
-			settings = {
-				json = {
-					-- Use schemastore.nvim for JSON schemas
-					schemas = require("schemastore").json.schemas(),
-					-- Enable validation
-					validate = { enable = true },
-				},
-			},
-		})
-	end,
+	lazy = true,
+	-- The schema settings are now handled in lspconfig.lua's setup_handlers
 }
