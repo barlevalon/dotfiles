10d31d002634b6edff74f527e6507830bba004df
Alon Hearter <barlevalon@users.noreply.github.com>
2025-11-16 18:50:52 +0200

    remove omarchy-managed stuff

--- .config/waybar/scripts/tray-separator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Check if any tray icons exist by looking for tray windows
-if [ -n "$(hyprctl clients -j | jq -r '.[] | select(.class | startswith("tray")) | .class')" ]; then
-    echo " | "
-else
-    echo ""
-fi
