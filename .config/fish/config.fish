0f22a034fb2347778047e97ca5ca6ec39593ea11
Alon Hearter <barlevalon@users.noreply.github.com>
2026-01-07 12:28:43 +0200

    fish: set UTF-8 locale for SSH compatibility

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,6 @@
 # Fish shell configuration
 # All configuration has been modularized into conf.d/ directory
+
+# UTF-8 locale (required for Unicode/Nerd Font icons over SSH)
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
